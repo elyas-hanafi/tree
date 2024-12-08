0845c23a33ce329b8ecd259a43d45789fec12d83
elyas-hanafi <eliinsta79@gmail.com>
2024-12-08 15:47:06 +0330

    feat: update

--- src/app/_components/CustomNodeContent.tsx
+++ src/app/_components/CustomNodeContent.tsx
@@ -6,7 +6,7 @@ const CustomNodeContent = (props: any) => {
   return (
     <div className="h-[120px] w-full rounded-xl border border-[hsla(210,77%,51%,0.1)] bg-[#ffffff] bg-gradient-to-b from-[hsla(210,77%,51%,0.1)] from-[5%] via-[#fdfdfd] via-[80%] to-[#ffffff] to-[10%] p-3">
       <div className="w-full relative h-full">
-        <div className="absolute -top-[2px] left-0 mx-auto h-[2px] w-[90%]  bg-gradient-to-l from-[#ffffff46] via-[#349affc4]  to-[#ffffffa9]"></div>
+        <div className="absolute -top-[12px] left-0 mx-auto h-[2px] w-[90%]  bg-gradient-to-l from-[#ffffff46] via-[#349affc4]  to-[#ffffffa9]"></div>
         <div className="flex w-full justify-center">
           <div className="flex h-[50px] w-[50px] flex-col items-center justify-center rounded-full bg-[#ffffff]">
             <p className="font-title text-xs">LEVEL</p>
