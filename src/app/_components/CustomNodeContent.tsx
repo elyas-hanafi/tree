06378ed88e69b77cad74cb30e7ec4abdb5f2cb33
elyas-hanafi <eliinsta79@gmail.com>
2024-12-08 15:38:54 +0330

    feat: update

--- src/app/_components/CustomNodeContent.tsx
+++ src/app/_components/CustomNodeContent.tsx
@@ -4,20 +4,22 @@ import React from "react";
 const CustomNodeContent = (props: any) => {
   // console.log(props.node.data.user.referralCode)
   return (
-    <div className="w-full rounded-xl border border-gray-soft bg-card p-3">
-      <div className="absolute -top-[2px] left-0 mx-auto h-[2px] w-[90%]  bg-gradient-to-l from-[#ffffff46] via-[#349affc4]  to-[#ffffffa9]"></div>
-      <div className="absolute -bottom-[2px] left-0 mx-auto h-[2px] w-[90%]  bg-gradient-to-l from-[#ffffff46] via-[#349affc4]  to-[#ffffffa9]"></div>
-      <div className="flex  bg-[#ffffff]">
-        <p className="font-title text-xs">LEVEL</p>
-        <p>${props.node.depth + 1}</p>
-      </div>
-      <div className="flex justify-between text-nowrap font-body capitalize">
-        <div>referral code: ${props.node?.data?.user?.referralCode || ""}</div>
-        <div
-          className="text-accent underline node-details"
-          data-node-id="${node.id}"
-        >
-          Details
+    <div className="h-[120px] w-full rounded-xl border border-[hsla(210,77%,51%,0.1)] bg-[#ffffff] bg-gradient-to-b from-[hsla(210,77%,51%,0.1)] from-[5%] via-[#fdfdfd] via-[80%] to-[#ffffff] to-[10%] p-3">
+      <div className="w-full relative h-full">
+        <div className="absolute -top-[2px] left-0 mx-auto h-[2px] w-[90%]  bg-gradient-to-l from-[#ffffff46] via-[#349affc4]  to-[#ffffffa9]"></div>
+        <div className="flex w-full justify-center">
+          <div className="flex h-[50px] w-[50px] flex-col items-center justify-center rounded-full bg-[#ffffff]">
+            <p className="font-title text-xs">LEVEL</p>
+            <p>{props.node.depth + 1}</p>
+          </div>
+        </div>
+        <div className="mt-5 flex items-center justify-between text-nowrap font-body capitalize">
+          <div className=" font-body text-[18px] font-medium">
+            referral code: {props?.node?.data?.user?.referralCode}
+          </div>
+          <div className="text-gray-soft underline hover:text-accent">
+            Details
+          </div>
         </div>
       </div>
     </div>
